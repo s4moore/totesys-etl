ccf115b6df2321a94bcc4711d438634d642252e0
maxMarty <m.c.foord1@gmail.com>
2025-02-24 15:20:22 +0000

    adding get credentials, function and tests

--- src/helper_functions/get_credentials.py
+++ src/helper_functions/get_credentials.py
@@ -2,18 +2,24 @@ import boto3
 import json
 from botocore.exceptions import ClientError
 
+
 def get_credentials(secret_name):
-    '''A function to return the aws rds credentials as a dictionary with keys of
+    """A function to return the aws rds credentials,
+    if secret successfully obtained then the secret is returned
+    as a python dictionary with keys of
     cohort_id, user, password, host, database and port
-    takes the name of the secret stored in secrets manager'''
+    otherwise an error is raised
+    A parameter is the name of the secret stored in secrets manager"""
 
-    client = boto3.client('secretsmanager', region_name='eu-west-2')
-    response = client.get_secret_value(SecretId=secret_name)
-    secret = response['SecretString']
-    credentials = json.loads(secret)
+    client = boto3.client("secretsmanager", region_name="eu-west-2")
 
-    return credentials
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        secret = response["SecretString"]
+        credentials = json.loads(secret)
 
-
-name = 'MySuperSecret'
-print(get_credentials(name))
+        return credentials
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
