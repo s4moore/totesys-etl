1fe0ba27b99c46edddcf1352eb697c170562cdff
SSM-F <melaniebc57@gmail.com>
2025-02-24 15:08:05 +0000

    changed name of folder to terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "BUCKET-NAME"
+    key= "terraform/state.tfstate"
+    region = "eu-west-2"
+  }
+}
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+        ProjectName = "Terrific Totes"
+        Team = "team-11-wicklow"
+        DeployedFrom = "Terraform"
+        Repository= "de-project"
+        Enviroment= "dev"
+    }
+  }
+}
