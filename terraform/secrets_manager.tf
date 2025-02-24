5b5f257ec471a6b622150556c1af48f63016b479
svet-g <svetlana.gljadelkina@gmail.com>
2025-02-24 15:19:17 +0000

    complete secrets manager, variables, caller id and iam

---
+++ terraform/secrets_manager.tf
@@ -0,0 +1,41 @@
+data "aws_caller_identity" "current" {}
+
+variable "cohort_id" {}
+variable "user" {}
+variable "password" {}
+variable "host" {}
+variable "database" {}
+variable "port" {}
+
+module "secrets_manager" {
+  source = "terraform-aws-modules/secrets-manager/aws"
+
+  # Secret
+  name        = "totesys"
+  description = "Totesys database credentials"
+  secret_string = jsonencode({ "cohort_id" : var.cohort_id,
+    "user" : var.user,
+    "password" : var.password,
+    "host" : var.host,
+    "database" : var.database,
+  "port" : var.port })
+
+  # Policy
+  create_policy = true
+  policy_statements = {
+    read = {
+      sid = "AllowAccountRead"
+      principals = [{
+        type        = "AWS"
+        identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      }]
+      actions   = ["secretsmanager:GetSecretValue"]
+      resources = ["*"]
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Terrific Totes"
+  }
+}
