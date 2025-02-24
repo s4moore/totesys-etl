4fe439ad4fa023ae82fdda262d813ad91a40ae25
SSM-F <melaniebc57@gmail.com>
2025-02-24 15:00:35 +0000

    s3 backend added and tags

--- terraform-p/provider.tf
+++ terraform-p/provider.tf
@@ -7,10 +7,19 @@ terraform {
   }
   backend "s3" {
     bucket = "BUCKET-NAME"
-    key= 
-    
+    key= "terraform/state.tfstate"
+    region = "eu-west-2"
   }
 }
 provider "aws" {
   region = "eu-west-2"
+  default_tags {
+    tags = {
+        ProjectName = "Terrific Totes"
+        Team = "team-11-wicklow"
+        DeployedFrom = "Terraform"
+        Repository= "de-project"
+        Enviroment= "dev"
+    }
+  }
 }
