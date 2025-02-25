ed8bea4cd1b5f5f1024d70308e0b959c9d26c3b3
SSM-F <melaniebc57@gmail.com>
2025-02-25 11:20:11 +0000

    sending eventbridge and setting cloudwatch metric alarm to detect custom log

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,19 +1,33 @@
-resource "aws_cloudwatch_event_rule" "every_20_min" {
-   name = "every-20-mins"
-   description = "Fires every 20 mins"
-   schedule_expression = "rate(20 minutes)"
+resource "aws_cloudwatch_log_group" "totes404TerraformGroup" {
+    name = "/aws/lambda/${aws_lambda_function.<lambda-name-handler>.function_name}"
+  
 }
-resource "aws_cloudwatch_event_target" "check_lambda_every_20_min" {
-    rule = aws_cloudwatch_event_rule.every_20_min.name
-    target_id = <insert lambda name here>
-    arn = aws_lambda_function.<lambda-name-here>.arn
+resource "aws_cloudwatch_metric_alarm" "error_raise" {
+    alarm_name = "totes404DataError"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = 1
+    metric_name = "Errors"
+    namespace = "AWS/Lambda"
+    period = 10
+    statistic = "Sum"
+    threshold = 0
+
+    alarm_description = "No new data to pull"
+    actions_enabled = true
+    alarm_actions = [ aws_sns_topic.<sns-name>.arn ]
+
+}
+
+resource "aws_cloudwatch_log_metric_filter" "metricFilterResource" {
+    name = "ErrorFilter"
+    pattern = "{($.eventName= ErrorInDataFlow) && ($.errorMessage = \"Something happened while trying to pull new data\")}"
+    log_group_name = aws_cloudwatch_log_group.totes404TerraformGroup.name
+
+    metric_transformation {
+      name = "TotesEvent"
+      namespace = "Totes/Errors"
+      value = "1"
+    }
   
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_function" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.<lambda-name-here>.<function-name-here>
-    principal = "events.amazonaws.com"
-    source_arn = aws_cloudwatch_event_rule.every_20_min.arn
-}
