d0faaafe6863e8605ac1c5ced5af0a6759deae7e
Samuel Moore <samuelallister@outlook.com>
2025-02-27 12:44:54 +0000

    add email var

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -5,7 +5,7 @@ resource "aws_sns_topic" "injest_lambda" {
   name = "injest_lambda-topic"
 }
 
-resource "aws_sns_topic_subscription" "injest_lambda-sub" {
+resource "aws_sns_topic_subscription" "injest_lambda_sub" {
   topic_arn = aws_sns_topic.injest_lambda.arn
   protocol  = "email"
   endpoint  = var.EMAIL
