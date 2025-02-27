120956718e80c0a9d8548fa3085b005d380a4fc1
Samuel Moore <samuelallister@outlook.com>
2025-02-27 09:18:54 +0000

    update week1_lambda

--- terraform/secrets_manager.tf
+++ terraform/secrets_manager.tf
@@ -1,44 +1,44 @@
 data "aws_caller_identity" "current" {}
 
-variable "cohort_id" {}
-variable "user" {}
-variable "password" {}
-variable "host" {}
-variable "database" {}
-variable "port" {}
+# variable "cohort_id" {}
+# variable "user" {}
+# variable "password" {}
+# variable "host" {}
+# variable "database" {}
+# variable "port" {}
 
-module "secrets_manager" {
-  source = "terraform-aws-modules/secrets-manager/aws"
+# module "secrets_manager" {
+#   source = "terraform-aws-modules/secrets-manager/aws"
 
-  # Secret
-  name        = "totesys-testing-8"
-  description = "Totesys database credentials"
-  secret_string = jsonencode({ "cohort_id" : "${var.cohort_id}",
-    "user" : "${var.user}",
-    "password" : "${var.password}",
-    "host" : "${var.host}",
-    "database" : "${var.database}",
-  "port" : var.port })
+#   # Secret
+#   name        = "totesys-testing-8"
+#   description = "Totesys database credentials"
+#   secret_string = jsonencode({ "cohort_id" : "${var.cohort_id}",
+#     "user" : "${var.user}",
+#     "password" : "${var.password}",
+#     "host" : "${var.host}",
+#     "database" : "${var.database}",
+#   "port" : var.port })
 
-  # Policy
-  create_policy = true
-  policy_statements = {
-    read = {
-      sid = "AllowAccountRead"
-      principals = [{
-        type        = "AWS"
-        identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-      }]
-      actions   = ["secretsmanager:GetSecretValue"]
-      resources = ["*"]
-    }
-  }
+#   # Policy
+#   create_policy = true
+#   policy_statements = {
+#     read = {
+#       sid = "AllowAccountRead"
+#       principals = [{
+#         type        = "AWS"
+#         identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+#       }]
+#       actions   = ["secretsmanager:GetSecretValue"]
+#       resources = ["*"]
+#     }
+#   }
 
-  tags = {
-    Environment = "Development"
-    Project     = "Terrific Totes"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#     Project     = "Terrific Totes"
+#   }
+# }
 
 resource "aws_secretsmanager_secret" "example" {
   name = "example"
