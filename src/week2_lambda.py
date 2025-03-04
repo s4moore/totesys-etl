d8098b356dd7de944262f8101dbca65ac4f731dd
Valeri Demotchkine <valdemotch@gmail.com>
2025-03-04 11:41:50 +0000

    adding lambda2

--- src/week2_lambda.py
+++ src/week2_lambda.py
@@ -11,6 +11,7 @@ from src.dim_staff import create_dim_staff
 from src.fact_sales_order import fact_sales_order
 from src.get_latest_file_as_df import get_latest_file_as_df
 from src.utils import collate_pkl_into_df, check_for_dim_date
+from layer2 import get_data, load_df_to_s3, tranform_file_into_df
 
 from datetime import datetime
 import logging
@@ -19,6 +20,8 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
+db_name = DB_NAME
+bucket_name = "totes-11-processed-data"
 
 def lambda_handler(event, context):
     """
@@ -44,68 +47,56 @@ def lambda_handler(event, context):
             match table:
                 case "sales_order":
                     logging.info("sales_order data transformation beginning")
-                    sales_df = get_latest_file_as_df(s3, pkl_files_written[table])
+                    sales_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
                     fact_sales = fact_sales_order(sales_df)
-                    fact_sales_pq = convert_to_parquet(fact_sales)
-                    pq_dict = upload_to_processing_bucket(
-                        s3, fact_sales_pq, "fact_sales_order"
-                    )
+                    pq_dict = load_df_to_s3(fact_sales, bucket_name, db_name, "fact_sales_order")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
                     )
                 case "staff":
                     logging.info("staff data transformation beginning")
-                    staff_df = get_latest_file_as_df(s3, pkl_files_written[table])
-                    dept_df = collate_pkl_into_df(s3, "department")
+                    staff_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
+                    dept_df = tranform_file_into_df(pkl_files_written["department"], bucket_name)
                     dim_staff = create_dim_staff(staff_df, dept_df)
-                    staff_pq = convert_to_parquet(dim_staff)
-                    pq_dict = upload_to_processing_bucket(s3, staff_pq, "dim_staff")
+                    pq_dict = load_df_to_s3(dim_staff, bucket_name, db_name, "dim_staff")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
                     )
                 case "address":
                     logging.info("address data transformation beginning")
-                    address_df = get_latest_file_as_df(s3, pkl_files_written[table])
+                    address_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
                     dim_loc_df = dim_location(address_df)
-                    loc_pq = convert_to_parquet(dim_loc_df)
-                    pq_dict = upload_to_processing_bucket(s3, loc_pq, "dim_location")
+                    pq_dict = load_df_to_s3(dim_loc_df, bucket_name, db_name, "dim_location")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
                     )
                 case "design":
                     logging.info("design data transformation beginning")
-                    design_df = get_latest_file_as_df(s3, pkl_files_written[table])
+                    design_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
                     dim_design_df = dim_design(design_df)
-                    design_pq = convert_to_parquet(dim_design_df)
-                    pq_dict = upload_to_processing_bucket(s3, design_pq, "dim_design")
+                    pq_dict = load_df_to_s3(dim_design_df, bucket_name, db_name,"dim_design")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
                     )
                 case "currency":
                     logging.info("currency data transformation beginning")
-                    currency_df = get_latest_file_as_df(s3, pkl_files_written[table])
+                    currency_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
                     dim_currency_df = dim_currency(currency_df)
-                    currency_pq = convert_to_parquet(dim_currency_df)
-                    pq_dict = upload_to_processing_bucket(
-                        s3, currency_pq, "dim_currency"
-                    )
+                    pq_dict = load_df_to_s3(dim_currency_df,bucket_name, db_name,"dim_currency")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
                     )
                 case "counterparty":
                     logging.info("counterparty data transformation beginning")
-                    counter_df = get_latest_file_as_df(s3, pkl_files_written[table])
-                    address_df = collate_pkl_into_df(s3, "address")
+                    counter_df = tranform_file_into_df(pkl_files_written[table], bucket_name)
+                    address_df = tranform_file_into_df(pkl_files_written["address"], bucket_name)
                     dim_counter_df = dim_counterparty(counter_df, address_df)
-                    dim_counter_pq = convert_to_parquet(dim_counter_df)
-                    pq_dict = upload_to_processing_bucket(
-                        s3, dim_counter_pq, "dim_counterparty"
-                    )
+                    pq_dict = load_df_to_s3(dim_counter_df, bucket_name, db_name,"dim_counterparty")
                     parquet_files_written.update(pq_dict)
                     logging.info(
                         f"{pkl_files_written[table]} transformed into {pq_dict}"
@@ -113,13 +104,13 @@ def lambda_handler(event, context):
                 case v:
                     logging.warning(f"Unexpected input in event: {v}")
 
-        if not check_for_dim_date(s3):
-            logging.info("creating dim_date")
-            dim_date_df = dim_date()
-            dim_date_pq = convert_to_parquet(dim_date_df)
-            pq_dict = upload_to_processing_bucket(s3, dim_date_pq, "dim_date")
-            parquet_files_written.update(pq_dict)
-            logging.info(f"{pq_dict} written to bucket")
+        # if not check_for_dim_date(s3):
+        #     logging.info("creating dim_date")
+        #     dim_date_df = dim_date()
+        #     dim_date_pq = convert_to_parquet(dim_date_df)
+        #     pq_dict = upload_to_processing_bucket(s3, dim_date_pq, "dim_date")
+        #     parquet_files_written.update(pq_dict)
+        #     logging.info(f"{pq_dict} written to bucket")
         return {"response": 200, "parquet_files_written": parquet_files_written}
 
     except Exception as e:
