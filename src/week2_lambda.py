590949d72f3acbd7aa3c1d3954f190bea736c0a2
maxMarty <m.c.foord1@gmail.com>
2025-03-04 08:17:20 +0000

    updating 20 minutes trigger to trigger the step function and amending transform lambda handler to test access to pkl files

--- src/week2_lambda.py
+++ src/week2_lambda.py
@@ -1,2 +1,2 @@
-def dummy_test_two():
-    pass
+def lambda_handler(event, context):
+    return f'this is a test to show the list of files {event['pkl_files_written']}'
--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -6,14 +6,18 @@ resource "aws_cloudwatch_event_rule" "every_20_min" {
 }
 resource "aws_cloudwatch_event_target" "check_lambda_every_20_min" {
     rule = aws_cloudwatch_event_rule.every_20_min.name
-    target_id = module.lambda_function.lambda_function_name
-    arn = module.lambda_function.lambda_function_arn
+    target_id = aws_sfn_state_machine.step_function_totes.name
+    arn = aws_sfn_state_machine.step_function_totes.arn
+    # target_id = module.lambda_function.lambda_function_name
+    # arn = module.lambda_function.lambda_function_arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_function" {
     statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = module.lambda_function.lambda_function_name
+    # action = "lambda:InvokeFunction"
+    action = "states:StartExecution"
+    # function_name = module.lambda_function.lambda_function_name
+    function_name = aws_sfn_state_machine.step_function_totes.name
     principal = "events.amazonaws.com"
     source_arn = aws_cloudwatch_event_rule.every_20_min.arn
 }
