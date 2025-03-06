dbb04dd8805af86ceb9a9c37cb8c9bbabac0e1d5
SSM-F <melaniebc57@gmail.com>
2025-03-06 12:04:12 +0000

    adding force destroy to buckets

--- terraform/p2_s3.tf
+++ terraform/p2_s3.tf
@@ -2,5 +2,6 @@ resource "aws_s3_bucket" "terrific-totes-processed" {
     bucket = "totes-11-processed-data"
     tags = {
         Name = "Terrific Totes bucket"
-    }  
+    }
+    force_destroy = true
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,5 +2,6 @@ resource "aws_s3_bucket" "terrific-totes-data" {
     bucket = "terrific-totes-data-team-11"
     tags = {
         Name = "Terrific Totes bucket"
-    }  
+    }
+    force_destroy = true
 }
