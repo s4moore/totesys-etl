f5f20298b37a9d7286781cffe850751543c3d8fb
Samuel Moore <samuelallister@outlook.com>
2025-03-06 17:11:26 +0000

    lambda3 needs some fixing

--- src/week3_lambda.py
+++ src/week3_lambda.py
@@ -1,2 +1,36 @@
-def lambda_handler():
-    pass
+import awswrangler as wr
+import pg8000
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def lambda_handler(event, context):
+    tables = ['fact_sales_order']
+    for table in tables:
+        df = wr.s3.read_parquet_table(database="load_db", table=table)
+        
+        con = pg8000.connect(
+            database= os.environ["PGDATABASE"],
+            user=os.environ["PGUSER"],
+            password=os.environ["PGPASSWORD"],
+            host=os.environ["PGHOST"],
+            port=5432
+            )
+
+        print(con)
+        
+        res = wr.postgresql.to_sql(
+            df=df,
+            con=con,
+            schema="public",
+            table=table,
+            mode="append",
+            index=False
+        )
+
+        print(res)
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
