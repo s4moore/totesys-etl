73f7db3598f1add7b6eedff81723035472a3af8f
svet-g <svetlana.gljadelkina@gmail.com>
2025-03-07 11:53:28 +0000

    add lambda mock testing and updates to integration tests

--- test/test_week2_lambda.py
+++ test/test_week2_lambda.py
@@ -2,9 +2,10 @@ from src.week2_lambda import lambda_handler
 import pytest
 import logging
 import boto3
+from unittest.mock import patch
 
 @pytest.fixture
-def event():
+def mock_event():
     return {
   "response": 200,
   "pkl_files_written": {
@@ -13,24 +14,51 @@ def event():
   "triggerLambda2": True
 }
 
-# integration testing
-class TestLambdaHandlerTransform:
-    def test_returns_dict_with_files_written(self, event):
-        output = lambda_handler(event, {})
-        parquet_files_written = {} # <<<INSERT FILE NAMES>>> #
-        expected = {"response": 200, "parquet_files_written": parquet_files_written}
-        assert output == expected
+@pytest.fixture
+def integration_event():
+    return {
+  "response": 200,
+  "pkl_files_written": {
+    "sales_order": "2025-03-07/10:58:10.330000/sales_order.pkl" # needs to have a path to a file currently in real s3 bucket
+  },
+  "triggerLambda2": True
+}
+
+# integration
+class TestLambdaHandlerTransformIntegration:
+    def test_returns_dict_with_files_written(self, integration_event):
+        output = lambda_handler(integration_event, {})
+        assert output["response"] == 200
+        assert 'fact_sales_order' in output["parquet_files_written"]
     
     def test_logs_error(self, caplog):
         with caplog.at_level(logging.INFO):
             output = lambda_handler({"triggerLambda2": True}, {})
             assert "Error running transform Lambda:" in caplog.text
     
-    def test_saves_pq_files_to_s3(self, event):
+    def test_saves_pq_files_to_s3(self, integration_event):
         s3 = boto3.client("s3")
-        lambda_handler(event, {})
-        response = s3.list_objects_v2(Bucket="totes-11-processed-data").get("Contents") # NONE
+        lambda_handler(integration_event, {})
+        response = s3.list_objects_v2(Bucket="totes-11-processed-data").get("Contents")
         bucket_files = [file["Key"] for file in response]
         for file in bucket_files:
-            assert "last_updated" in file
             assert ".parquet" in file
+
+# mocking
+class TestLambdaHandlerTransformMocking:
+    def test_uploads_file_to_s3(self, mock_event, mock_aws_with_buckets_and_glue):
+        s3, glue = mock_aws_with_buckets_and_glue
+        lambda_handler(mock_event, {})
+        response = s3.list_objects_v2(Bucket="totes-11-processed-data")
+        tables_written = [file["Key"].split("/")[0] for file in response["Contents"]]
+        assert "fact_sales_order" in tables_written
+
+    def test_returns_dict_with_files_written(self, mock_event, mock_aws_with_buckets_and_glue):
+        output = lambda_handler(mock_event, {})
+        assert output["response"] == 200
+        assert "/fact_sales_order/" in output["parquet_files_written"]['fact_sales_order']
+      
+    def test_logs_errors(self, mock_event, mocked_aws, caplog):
+        with caplog.at_level(logging.ERROR):
+          output = lambda_handler(mock_event, {})
+        assert "Error running transform Lambda:" in caplog.text
